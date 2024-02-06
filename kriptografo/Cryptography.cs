720b4cf9ec037d1eb33baf7cdea3c91189a6147a
obrusilva <brunopdp@gmail.com>
2024-02-06 19:52:23 -0300

    :ok_hand: Melhoria criptografia

--- kriptografo/Cryptography.cs
+++ kriptografo/Cryptography.cs
@@ -4,29 +4,34 @@ using System.Text;
 
 namespace kriptografo
 {
-     //# Gerar chave RSA de 2048 bits
-      //    openssl genrsa -out rsa-private-key.pem 2048
+    //# Gerar chave RSA de 2048 bits
+    //    openssl genrsa -out rsa-private-key.pem 2048
 
     public class Cryptography(IConfiguration configuration)
     {
         private readonly RSA _rsa = RSA.Create();
-        private readonly string _privateKey = configuration["rsa-private-key.pem"];
+        private readonly string _privateKey = Encoding.UTF8.GetString(Convert.FromBase64String(configuration["rsa-private-key"]));
+
 
         public string Encrypt(string text)
         {
             _rsa.ImportFromPem(_privateKey);
-            var encrypt = _rsa.Encrypt(Encoding.UTF8.GetBytes(text), RSAEncryptionPadding.Pkcs1);
-            string encryptText =  BitConverter.ToString(encrypt);
-            return encryptText;       
-         }
+            var biteMsg = Encoding.UTF8.GetBytes(text);
+            var encrypt = _rsa.Encrypt(biteMsg, RSAEncryptionPadding.Pkcs1);
+            string encryptText = Convert.ToBase64String(encrypt);
+            return encryptText;
+        }
 
         public string Decrypt(string text)
         {
+
+            _rsa.ImportFromPem(_privateKey);
             string decrypt = text;
             try
             {
-                var decryptByte = _rsa.Decrypt(Encoding.UTF8.GetBytes(text), RSAEncryptionPadding.Pkcs1);
-                decrypt =  Encoding.UTF8.GetString(decryptByte);
+                var textBite = Convert.FromBase64String(text);
+                var decryptByte = _rsa.Decrypt(textBite, RSAEncryptionPadding.Pkcs1);
+                decrypt = Encoding.UTF8.GetString(decryptByte);
                 return decrypt;
             }
             catch
